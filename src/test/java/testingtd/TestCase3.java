f8a3770f9795c129cbadd57aa77a4fdc1d7a116d
Aika <asemka-2012@mail.ru>
2018-07-27 02:34:39 -0400

    newclass

---
+++ src/test/java/testingtd/TestCase3.java
@@ -0,0 +1,24 @@
+package testingtd;
+
+public class TestCase3 {
+	
+//	// String url = ConfigLoader.getMyValue("a key in here");
+//	// driver.get("url")
+//	
+//	
+//	
+//	public static void main(String[] args) {
+//		
+//		Properties prop = new Properties();
+//		
+//		try {
+//		FileInputStream fis = new FileInputStream("config2.properties");
+//		prop.load(fis);
+//		} catch (IOException e) {
+//		e.printStackTrace();
+//	}
+//		System.out.println( prop.getProperty("SamBennet") );
+//	}
+//	
+
+}
